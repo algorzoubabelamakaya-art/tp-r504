fdf53fc53f064ddc3d5b0ece74abc9b67d238ac7
Algor ZOUBABELA <algor.zoubabela-makaya@iniv-rouen.fr>
2026-09-16 14:02:04 +0200

    Q4.6 - gestion cas 0 puissance negative

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,12 +1,14 @@
-#question 2.2 
+# question 4.6 - version active
 def puissance(a, b):
-    return a ** b
-
-#question 2.4
-
-def puissance(a, b):
-    if not type(a) is int:
+    # on vérifie que a ET b sont bien des entiers (int)
+    # si l'un des deux ne l'est pas, on déclenche une erreur TypeError
+    if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
-    if not type(b) is int:
-        raise TypeError("Only integers are allowed")
+
+    # cas particulier : 0 à une puissance négative (ex: 0^-1 = 1/0)
+    # c'est mathématiquement indéfini, donc on lève une exception volontairement
+    if a == 0 and b < 0:
+        raise Exception("0 à une puissance négative est indéfini")
+
+    # si tout est bon, on calcule et renvoie a élevé à la puissance b
     return a ** b
