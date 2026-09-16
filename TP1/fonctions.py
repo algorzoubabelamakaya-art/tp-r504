3ec258ec1dce37fd9e326422e630e3d97b273fd9
Algor ZOUBABELA <algor.zoubabela-makaya@iniv-rouen.fr>
2026-09-16 14:15:38 +0200

    Partie 5 - puissance sans operateur ** (historique en commentaire)

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,11 +1,30 @@
+# Q2.2 - première version 
+# def puissance(a, b):
+#     return a ** b
+
+# Q2.4 - avec vérification de type 
+# def puissance(a, b):
+#     if not type(a) is int:
+#         raise TypeError("Only integers are allowed")
+#     if not type(b) is int:
+#         raise TypeError("Only integers are allowed")
+#     return a ** b
+
+# Q4.6 - avec gestion du cas 0 puissance négative 
+# def puissance(a, b):
+#     if not type(a) is int or not type(b) is int:
+#         raise TypeError("Only integers are allowed")
+#     if a == 0 and b < 0:
+#         raise Exception("0 à une puissance négative est indéfini")
+#     return a ** b
+
+# Partie 5 
 def puissance(a, b):
     # on vérifie que a ET b sont bien des entiers (int)
-    # si l'un des deux ne l'est pas, on déclenche une erreur TypeError
     if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
 
-    # cas particulier : 0 à une puissance négative (ex: 0^-1 = 1/0)
-    # c'est mathématiquement indéfini, donc on lève une exception volontairement
+    # cas particulier : 0 à une puissance négative (ex: 0^-1 = 1/0) -> indéfini
     if a == 0 and b < 0:
         raise Exception("0 à une puissance négative est indéfini")
 
