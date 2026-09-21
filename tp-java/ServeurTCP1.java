a874a72fccaa07915bf73cf9290cb0edaab9df3a
Algor ZOUBABELA <algor.zoubabela-makaya@iniv-rouen.fr>
2026-09-21 13:54:26 +0200

    Q3.3 - communication bidirectionnelle TCP (chaine inversee)

--- tp-java/ServeurTCP1.java
+++ tp-java/ServeurTCP1.java
@@ -1,3 +1,5 @@
+//Server TCP1
+
 import java.io.*;
 import java.net.*;
 
--- tp-java/ServeurTCP3.java
+++ tp-java/ServeurTCP3.java
@@ -1,9 +1,9 @@
-//SErveur TCP2
+//server TCP3
 
 import java.io.*;
 import java.net.*;
 
-public class ServeurTCP2
+public class ServeurTCP3
 {
 	public static void main( String[] args )
 	{
@@ -16,7 +16,12 @@ public class ServeurTCP2
 				Socket socket = socketserver.accept();
 				System.out.println( "Connection d'un client" );
 				DataInputStream dIn = new DataInputStream( socket.getInputStream() );
-				System.out.println( "Message: " + dIn.readUTF() );
+				String msg = dIn.readUTF();
+				System.out.println( "Message: " + msg );
+
+				String rev = new StringBuilder(msg).reverse().toString();
+				DataOutputStream dOut = new DataOutputStream( socket.getOutputStream() );
+				dOut.writeUTF( rev );
 
 				socket.close();
 			}
